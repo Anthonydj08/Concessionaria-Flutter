3999d506a986d10858172c1279b5cbe2423a6ead
Anthony Douglas <31930381+Anthonydj08@users.noreply.github.com>
2021-05-30 20:47:21 -0300

    Alteração de cor

--- concessionaria_flutter/lib/main.dart
+++ concessionaria_flutter/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.lightBlue,
+        primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Concessionária Teste 2'),
     );
