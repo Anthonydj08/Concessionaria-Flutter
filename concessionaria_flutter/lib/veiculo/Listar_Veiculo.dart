4c02a4952f57923bd2667a40e7e36a7dae2f00da
Joomonteiro <56613291+Joomonteiro@users.noreply.github.com>
2021-07-19 23:13:37 -0300

    Criado o CRUD Relacional Completo para Veículo
    Criado o CRUD Relacional Completo para Veículo
    Disponível na aba Veículos -> Botão adicionar

--- concessionaria_flutter/lib/veiculo/Listar_Veiculo.dart
+++ concessionaria_flutter/lib/veiculo/Listar_Veiculo.dart
@@ -1,3 +1,6 @@
+import 'package:concessionaria_flutter/db/database.dart';
+import 'package:concessionaria_flutter/entitys/veiculo_entity.dart';
+
 import 'package:concessionaria_flutter/home/Home.dart';
 import 'package:concessionaria_flutter/model/veiculo/VeiculoModel.dart';
 import 'package:concessionaria_flutter/veiculo/Cadastrar_Veiculo.dart';
@@ -7,12 +10,17 @@ import 'package:flutter/material.dart';
 class ListarVeiculo extends StatefulWidget {
   final List<VeiculoModel> veiculos = [];
   static String routeName = "/listarVeiculo";
-
+  ListarVeiculo({Key? key,required this.db, this.veiculo}) : super(key: key);
+  final AppDatabase db;
+  VeiculoEntity? veiculo;
   @override
-  _ListarVeiculoState createState() => _ListarVeiculoState();
+  _ListarVeiculoState createState() => _ListarVeiculoState(this.db,this.veiculo);
 }
 
 class _ListarVeiculoState extends State<ListarVeiculo> {
+  final AppDatabase db;
+  VeiculoEntity? veiculo;
+  _ListarVeiculoState(this.db,this.veiculo);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +29,8 @@ class _ListarVeiculoState extends State<ListarVeiculo> {
           child: appBarVeiculo('Veículos', Home.routeName, context),
         ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).pushNamed(CadastrarVeiculo.routeName);
+        onPressed: () async{
+          await Navigator.of(context).pushNamed(CadastrarVeiculo.routeName);
         },
         child: const Icon(Icons.add),
         backgroundColor: Color.fromRGBO(0, 184, 160, 1),
@@ -30,20 +38,56 @@ class _ListarVeiculoState extends State<ListarVeiculo> {
       body: Container(
         height: MediaQuery.of(context).size.height,
         color: Color.fromARGB(255, 41, 45, 50),
-        child: Container(
-          width: MediaQuery.of(context).size.height,
-          child: Form(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [],
-                ),
-              ),
-            ),
-          ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FutureBuilder<List<VeiculoEntity>>(
+                      future: db.veiculoRepositoryDao.getAll(),
+                      builder: (context, snapshot) {
+                        return snapshot.hasData? ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context,index){
+                          return Card(
+                            color: Color.fromARGB(10 ,205, 205, 205),
+                            child: ListTile(
+                              onTap: () async {
+                                var result =await Navigator.push(context, 
+                                MaterialPageRoute(builder: (context) {
+                                  return CadastrarVeiculo(db: db, veiculo: snapshot.data![index]);
+                                  }, 
+                                ),
+                                );
+                                if(result){
+                                  setState(() {});
+                                } 
+                                // veiculo = snapshot.data![index];
+                                // await Navigator.of(context).pushNamed(CadastrarVeiculo.routeName);
+                              },
+                              title: Text( snapshot.data![index].modelo),
+                              subtitle: Text(snapshot.data![index].ano),
+
+                            ),
+                          );
+                        },
+                      ): Center(child: Text('Não há Veiculos...'),
+                        );
+                      }
+                    ),
         ),
       ),
-    );
+                );
+                // Column(
+                //   children: [],
+                // ),
+     
   }
 }
+// Container(
+//         height: MediaQuery.of(context).size.height,
+//         color: Color.fromARGB(255, 41, 45, 50),
+//         child: Container(
+//           width: MediaQuery.of(context).size.height,
+//           child: Form(
+//             child: Padding(
+//               padding: const EdgeInsets.all(8.0),
+//               child: SingleChildScrollView(
+//                 child: 
